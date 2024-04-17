e0123f8d8266d12b1a69ca27709312f6318b6d9f
Dustin Fang <20fangd@gmail.com>
2024-04-17 16:25:53 -0400

    Initial Commit

---
+++ BudgetWise/BudgetWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetWiseApp.swift
+//  BudgetWise
+//
+//  Created by Dustin Fang on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
